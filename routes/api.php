15eec4c71bef0d3bacb1065a9eea2147715c0e93
blanzadeannsamuel-crypto <blanzadeannsamuel@gmail.com>
2026-02-25 16:38:52 +0800

    Revert "Merge pull request #2 from blanzadeann/Developer-8-Ferrer"
    This reverts commit bdfdbc73a74e3a6abfcf37be8fd0a59027e6a08b, reversing
    changes made to a1374c47931b7104d563b2b1b7a878666d9e6308.

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,9 @@
 <?php
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\NewsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-//FERRER
-Route::get('/news', [NewsController::class, 'index']);
