581677651e154778a4b3f05b18e2c25a3eb15915
superkrnch <karlliam126@gmail.com>
2026-02-25 15:45:19 +0800

    Initial commit on Developer-13-Valdez branch

---
+++ app/Http/Requests/ApiRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class ApiRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true; // Default to true for API requests
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     * * @param Validator $validator
+     * @throws HttpResponseException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => 'Validation errors',
+            'errors'  => $validator->errors()
+        ], 422));
+    }
+}
