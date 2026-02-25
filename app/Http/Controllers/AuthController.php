a1374c47931b7104d563b2b1b7a878666d9e6308
blanzadeannsamuel-crypto <blanzadeannsamuel@gmail.com>
2026-02-25 16:05:38 +0800

    fixed login create token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,23 +7,32 @@ use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use Illuminate\Http\Request;
 
+
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        
+
     }
 
     public function login(Request $request)
-    {
-        if (!Auth::attempt($request->only('email', 'password'))) {
+        {
+            
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
             return response()->json([
                 'message' => 'Invalid credentials'
             ], 401);
         }
 
-        $user = Auth::user();
-        $token = $user->createToken('auth_token')->plainTextToken;
+        $token = $user->createToken('api_token')->plainTextToken;
+
         return response()->json([
             'message' => 'Login successful',
             'token' => $token
