1357daa4c5e4c5b68a25b3ebc94c4dd3426124aa
blanzadeannsamuel-crypto <blanzadeannsamuel@gmail.com>
2026-02-25 15:36:56 +0800

    login and logout for auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->create('apitoken')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Login successful',
+            'token' => $token
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Logged out successfully'
+        ]);
+    } 
+}
