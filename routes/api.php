1f526db6b478fb640e529cc770122a8f70eb2c36
blanzadeannsamuel-crypto <blanzadeannsamuel@gmail.com>
2026-03-04 16:03:22 +0800

    initial

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+route::post('login', [AuthController::class, 'login']);
+route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
